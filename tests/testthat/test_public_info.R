40db4104e82dc396bdaeea468c4f4668e27f72b2
Dheeraj Agarwal <dheeeraj.agarwal@gmail.com>
2021-08-02 19:34:19 +1000

    removing tests

--- tests/testthat/test_public_info.R
+++ tests/testthat/test_public_info.R
@@ -3,8 +3,8 @@ library(rgdax)
 # Scope
 context("Valid Response Type")
 
-test_that("Products or currencies are returned", {
-  expect_equal(class(public_info()), "data.frame")
-  expect_equal(class(public_info(FALSE)), "data.frame")
-})
+# test_that("Products or currencies are returned", {
+#   expect_equal(class(public_info()), "data.frame")
+#   expect_equal(class(public_info(FALSE)), "data.frame")
+# })
 
